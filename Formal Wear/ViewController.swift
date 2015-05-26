98890b2f59ab1e59e43162b7b2dbb4203867c551
johnbergeson <john.bergeson@menloschool.org>
2015-05-25 17:44:12 -0700

    Initial Commit

---
+++ Formal Wear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Formal Wear
+//
+//  Created by John Bergeson on 5/25/15.
+//  Copyright (c) 2015 John Bergeson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
