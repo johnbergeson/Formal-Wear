45ca95fd032f819c647e1879d5748fc2f337be07
johnbergeson <john.bergeson@menloschool.org>
2015-05-28 19:57:12 -0700

    UIImage Photo Album Access
    Everything works except for some reason the AddDressViewController
    isn’t showing my buttons and text labels

--- Formal Wear/AddDressViewController.swift
+++ Formal Wear/AddDressViewController.swift
@@ -8,9 +8,34 @@
 
 import Foundation
 import UIKit
-class AddDressViewController : UIViewController
+class AddDressViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
+    var photoImageView = UIImageView(frame: CGRectMake(40, 120, 200, 200))
+    
   override func viewDidLoad() {
     self.view.backgroundColor = UIColor.whiteColor()
+    self.view.addSubview(photoImageView)
   }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        self.view.addSubview(photoImageView)
+    }
+    @IBAction func openPhotoLibrary(sender: AnyObject)
+    {
+        var photoPicker = UIImagePickerController()
+        photoPicker.delegate = self
+        photoPicker.sourceType = .PhotoLibrary
+        self.presentViewController(photoPicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject])
+    {
+        photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismissViewControllerAnimated(false, completion: nil)
+    }
+        
+
 }
+
